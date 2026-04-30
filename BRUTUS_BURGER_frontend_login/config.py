1ac34bb9db454525e54b3b0ba681e4ae4603841f
davinsonvargas04-spec <davinsonvargas04@gmail.com>
2026-04-29 23:51:45 -0500

    Update config.py

--- BRUTUS_BURGER_frontend_login/config.py
+++ BRUTUS_BURGER_frontend_login/config.py
@@ -1,10 +1,6 @@
 import os
 
-
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-change-me")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "mysql+pymysql://root:1234@localhost/tienda_contabilidad",
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
